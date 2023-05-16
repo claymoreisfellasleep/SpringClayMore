85f8aec852b9413cc33c1641cf99da192b55df55
clay <claymoreisfellasleep@gmail.com>
2023-05-17 03:43:40 +0600

    Adding student model

---
+++ src/main/java/clay/more/models/Student.java
@@ -0,0 +1,19 @@
+package clay.more.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+
+public class Student {
+    private Long id;
+    private String name;
+    private String surname;
+    private int exam;
+    private String mark;
+}
